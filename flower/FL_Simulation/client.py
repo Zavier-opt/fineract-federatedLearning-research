111f7657d53df50c286be5d01025b59f13e2d404
Ziwei <ziweihans@gmail.com>
2022-07-20 01:04:12 -0400

    update flower FL model

--- flower/FL_Simulation/client.py
+++ flower/FL_Simulation/client.py
@@ -1,19 +1,17 @@
-import  torch as t
+import torch as t
 import numpy as np
 from sklearn.metrics import accuracy_score
 import flwr as fl
 from collections import OrderedDict
-import torch.nn as nn
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-german = pd.read_csv("../data/german.csv")  
-test_size = 0.20
-processed_data = None
-categorical = None
-label_encoders = {}
+from socket import socket, AF_INET, SOCK_STREAM
+import sys
+import pickle
+
 
 def preprocessing(dataset, data, test_size):
     """
@@ -24,23 +22,22 @@ def preprocessing(dataset, data, test_size):
     data: DataFrame
         Pandas dataframe containing German dataset.
     """
-    
+
     global processed_data
     global categorical
     global label_encoders
 
     # Reset global variables
-    
+
     processed_data = None
     categorical = None
     label_encoders = {}
 
-
     if dataset == "German":
         # Drop savings account and checkings account columns as they contain a lot
         # of NaN values and may not always be available in real life scenarios
-        data = data.drop(columns = ['Saving accounts', 'Checking account'])
-        
+        data = data.drop(columns=['Saving accounts', 'Checking account'])
+
     dat_dict = data.to_dict()
     new_dat_dict = {}
 
@@ -55,7 +52,6 @@ def preprocessing(dataset, data, test_size):
 
     data = pd.DataFrame.from_dict(new_dat_dict)
     del new_dat_dict
-
 
     # print(data.describe())
     # print(data.describe(include='O'))
@@ -74,8 +70,9 @@ def preprocessing(dataset, data, test_size):
         label_encoders[category] = le
 
     for col in data.columns:
-        if(col not in categorical):
-            data[col] = (data[col].astype('float') - np.mean(data[col].astype('float')))/np.std(data[col].astype('float'))
+        if (col not in categorical):
+            data[col] = (data[col].astype('float') - np.mean(data[col].astype('float'))) / np.std(
+                data[col].astype('float'))
 
     # print(data.describe())
     # print(data.describe(include='O'))
@@ -100,8 +97,7 @@ def preprocessing(dataset, data, test_size):
         x = data.drop('class', axis=1)
         y = data['class'].astype('int')
 
-
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = test_size)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=test_size)
     x_train = pd.DataFrame(x_train)
     y_train = pd.DataFrame(y_train)
     y_test = pd.DataFrame(y_test)
@@ -109,36 +105,32 @@ def preprocessing(dataset, data, test_size):
     sc = StandardScaler()
     x_train = sc.fit_transform(x_train)
     x_test = sc.transform(x_test)
-    
+
     y_train = y_train[y_train.columns[0]].to_numpy()
     y_test = y_test[y_test.columns[0]].to_numpy()
 
     return (x_train, x_test, y_train, y_test)
 
-X_train, X_test, y_train, y_test = preprocessing("German", german, test_size)
-X_train_tensor = t.FloatTensor(X_train)
-y_train_tensor = t.tensor(y_train,dtype=t.long)
-X_test_tensor = t.FloatTensor(X_test)
-y_test_tensor = t.tensor(y_test,dtype=t.long)
 
 class Net(t.nn.Module):
-    def __init__(self,input_channels,output_channels):
-        super(Net,self).__init__()
-        #Our network:
+    def __init__(self, input_channels, output_channels):
+        super(Net, self).__init__()
+        # Our network:
         # Linear1->relu->Batchnorm->Linear2->relu->Batchnorm->Dropout->Linear3->output
         # Softmax is added in the predict function
-        #This applies Linear transformation to input data. 
-        self.fc1 = t.nn.Linear(input_channels,int(1.5*input_channels))
-        self.fc2 = t.nn.Linear(int(1.5*input_channels),int(1.5*input_channels))
-        self.fc3 = t.nn.Linear(int(1.5*input_channels),output_channels)
-        
+        # This applies Linear transformation to input data.
+        self.fc1 = t.nn.Linear(input_channels, int(1.5 * input_channels))
+        self.fc2 = t.nn.Linear(int(1.5 * input_channels), int(1.5 * input_channels))
+        self.fc3 = t.nn.Linear(int(1.5 * input_channels), output_channels)
+
         self.relu = t.nn.ReLU()
         self.dropout = t.nn.Dropout(p=0.1)
-        self.batchnorm1 = t.nn.BatchNorm1d(int(1.5*input_channels))
-        self.batchnorm2 = t.nn.BatchNorm1d(int(1.5*input_channels))
+        self.batchnorm1 = t.nn.BatchNorm1d(int(1.5 * input_channels))
+        self.batchnorm2 = t.nn.BatchNorm1d(int(1.5 * input_channels))
         self.sigmoid = t.nn.Sigmoid()
-    #This must be implemented
-    def forward(self,x):
+
+    # This must be implemented
+    def forward(self, x):
         x = self.relu(self.fc1(x))
         x = self.batchnorm1(x)
         x = self.relu(self.fc2(x))
@@ -148,53 +140,39 @@ class Net(t.nn.Module):
         x = self.sigmoid(x)
         return x
 
-    def predict(self,x):
+    def predict(self, x):
         output = self.forward(x)
-        prediction = t.argmax(output,1)
+        prediction = t.argmax(output, 1)
         return prediction
 
 
-def classifier_train(epochs, model, optimizer, X,y,criterion):
+def classifier_train(epochs, model, optimizer, X, y, criterion):
     losses = []
     for i in range(epochs):
-        #Precit the output for Given input
+        # Precit the output for Given input
         y_pred = model.forward(X)
-        #Compute Cross entropy loss
-        loss = criterion(y_pred,y)
-        #Add loss to the list
+        # Compute Cross entropy loss
+        loss = criterion(y_pred, y)
+        # Add loss to the list
         losses.append(loss.item())
-        
-        #Print loss
-        if i%500==0:
-            print("Epoch:",i," Loss:",loss.item())
-        
-        #Clear the previous gradients
+
+        # Print loss
+        if i % 500 == 0:
+            print("Epoch:", i, " Loss:", loss.item())
+
+        # Clear the previous gradients
         optimizer.zero_grad()
-        #Compute gradients
+        # Compute gradients
         loss.backward()
-        #Adjust weights
+        # Adjust weights
         optimizer.step()
     return losses
 
 
-
-# Define the input and output
-input_channels = X_train_tensor.shape[1]
-output_channels = 2
-
-#Initialize the model        
-net = Net(input_channels,output_channels)
-#Define loss criterion
-criterion = t.nn.CrossEntropyLoss()
-#Define the optimizer
-optimizer = t.optim.Adam(net.parameters(), lr=0.001)
-#Number of epochs
-epochs = 5000
-
-
-def accuracy(model,X,y):
+def accuracy(model, X, y):
     output_labels = model.forward(X)
-    return criterion(output_labels, y), accuracy_score(model.predict(X),y)
+    return criterion(output_labels, y), accuracy_score(model.predict(X), y)
+
 
 class ClassifierClient(fl.client.NumPyClient):
     def get_parameters(self):
@@ -207,14 +185,90 @@ class ClassifierClient(fl.client.NumPyClient):
 
     def fit(self, parameters, config):
         self.set_parameters(parameters)
-        classifier_train(epochs, net, optimizer,X_train_tensor,y_train_tensor,criterion)
-        return self.get_parameters(),  X_train_tensor.shape[0], {}
+        classifier_train(epochs, net, optimizer, X_train_tensor, y_train_tensor, criterion)
+        return self.get_parameters(), X_train_tensor.shape[0], {}
 
     def evaluate(self, parameters, config):
         self.set_parameters(parameters)
-        loss, acc = accuracy(net ,X_test_tensor,y_test_tensor)
-        return float(loss),  X_test_tensor.shape[0], {"accuracy": float(acc)}
+        loss, acc = accuracy(net, X_test_tensor, y_test_tensor)
+        return float(loss), X_test_tensor.shape[0], {"accuracy": float(acc)}
 
-fl.client.start_numpy_client("[::]:5000", client=ClassifierClient())
+
+if __name__ == '__main__':
+
+    IP = '127.0.0.1'
+    SERVER_PORT = 50000
+    BUFLEN = 1024
+
+    dataSocket = socket(AF_INET, SOCK_STREAM)
+    try:
+        dataSocket.connect((IP, SERVER_PORT))
+    except:
+        print("Unsuccessfully connect to the server, check the address and port")
+        sys.exit(1)
+
+    command = input("Train or Predict ?: ")
+    dataSocket.send(command.encode())
+
+    # Preparing Data
+    german = pd.read_csv("../data/german.csv")
+    test_size = 0.20
+    processed_data = None
+    categorical = None
+    label_encoders = {}
+
+    X_train, X_test, y_train, y_test = preprocessing("German", german, test_size)
+    X_train_tensor = t.FloatTensor(X_train)
+    y_train_tensor = t.tensor(y_train, dtype=t.long)
+    X_test_tensor = t.FloatTensor(X_test)
+    y_test_tensor = t.tensor(y_test, dtype=t.long)
+
+    # Define the input and output
+    input_channels = X_train_tensor.shape[1]
+    output_channels = 2
+
+    # Initialize the model
+    net = Net(input_channels, output_channels)
+    # Define loss criterion
+    criterion = t.nn.CrossEntropyLoss()
+    # Define the optimizer
+    optimizer = t.optim.Adam(net.parameters(), lr=0.001)
+    # Number of epochs
+    epochs = 5000
+
+    if command == "Train":
+
+
+        print(">>> Connect Flower server")
+        fl.client.start_numpy_client("[::]:8080", client=ClassifierClient())
+
+        # for name, param in net.named_parameters():
+        #     if param.requires_grad:
+        #         print(name, param.data)
+
+        print(">>> Send Model to Server")
+        serialized_model = pickle.dumps(net.state_dict())
+        dataSocket.send(serialized_model)
+
+        print(">>> Training Finish")
+
+
+    elif command == "Predict":
+        new_model_dict_string = dataSocket.recv(BUFLEN*100)
+        new_model_dict = pickle.loads(new_model_dict_string)
+        net.load_state_dict(new_model_dict)
+        with t.no_grad():
+            net.eval()
+            y_pred_vector = net(X_test_tensor)
+            val_loss = criterion(y_pred_vector, y_test_tensor)
+            y_pred = net.predict(X_test_tensor)
+
+            acc = (y_test_tensor == y_pred).sum()/ y_test_tensor.size(0)
+
+
+        print("Prediction Result:", y_pred)
+        print("Score:",val_loss)
+        print("Accuracy:", acc)
+
 
 
--- flower/FL_Simulation/server.py
+++ flower/FL_Simulation/server.py
@@ -2,6 +2,44 @@ from typing import List, Tuple
 
 import flwr as fl
 from flwr.common import Metrics
+from socket import socket, AF_INET, SOCK_STREAM
+import pickle
+import torch as t
+from collections import OrderedDict
+
+
+class Net(t.nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super(Net, self).__init__()
+        # Our network:
+        # Linear1->relu->Batchnorm->Linear2->relu->Batchnorm->Dropout->Linear3->output
+        # Softmax is added in the predict function
+        # This applies Linear transformation to input data.
+        self.fc1 = t.nn.Linear(input_channels, int(1.5 * input_channels))
+        self.fc2 = t.nn.Linear(int(1.5 * input_channels), int(1.5 * input_channels))
+        self.fc3 = t.nn.Linear(int(1.5 * input_channels), output_channels)
+
+        self.relu = t.nn.ReLU()
+        self.dropout = t.nn.Dropout(p=0.1)
+        self.batchnorm1 = t.nn.BatchNorm1d(int(1.5 * input_channels))
+        self.batchnorm2 = t.nn.BatchNorm1d(int(1.5 * input_channels))
+        self.sigmoid = t.nn.Sigmoid()
+
+    # This must be implemented
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.batchnorm1(x)
+        x = self.relu(self.fc2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.fc3(x)
+        x = self.sigmoid(x)
+        return x
+
+    def predict(self, x):
+        output = self.forward(x)
+        prediction = t.argmax(output, 1)
+        return prediction
 
 
 # Define metric aggregation function
@@ -14,12 +52,69 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     return {"accuracy": sum(accuracies) / sum(examples)}
 
 
-# Define strategy
-strategy = fl.server.strategy.FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
+def update_basic_model(basic_net, received_model_dict, TOTAL_CLIENT_COUNT):
+    basic_net_dict = basic_net.state_dict()
+    avg_updates = OrderedDict()
+    for key in basic_net_dict.keys():
+        avg_updates[key] = ((basic_net_dict[key]) * (TOTAL_CLIENT_COUNT - 1) + (
+        received_model_dict[key])) / TOTAL_CLIENT_COUNT
+    basic_net.load_state_dict(avg_updates)
 
-# Start Flower server
-fl.server.start_server(
-    server_address="[::]:5000",
-    config={"num_rounds": 3},
-    strategy=strategy,
-)
+    # for name, param in basic_net.named_parameters():
+    #     if param.requires_grad:
+    #         print(name, param.data)
+
+
+if __name__ == '__main__':
+    IP = '127.0.0.1'
+    PORT = 50000
+    BUFLEN = 1024
+
+    listenSocket = socket(AF_INET, SOCK_STREAM)
+    listenSocket.bind((IP, PORT))
+
+    listenSocket.listen(8)
+    print(f'>>> Sever launch successfully, wait for a Client to connect at port :{PORT}...')
+
+    TOTAL_CLIENT_COUNT = 0
+    input_channels = 8
+    output_channels = 2
+    basic_net = Net(input_channels, output_channels)
+
+    while True:
+        dataSocket, addr = listenSocket.accept()
+        addr = str(addr)
+        print(f'>>> Client at {addr} , connect successfully')
+
+        command = dataSocket.recv(BUFLEN).decode()
+        if command == "Train":
+            TOTAL_CLIENT_COUNT = TOTAL_CLIENT_COUNT + 1
+
+            print(">>> Start Flower server")
+            # Define strategy
+            strategy = fl.server.strategy.FedAvg(min_fit_clients=1, min_available_clients=1,
+                                                 evaluate_metrics_aggregation_fn=weighted_average)
+            fl.server.start_server(
+                server_address="[::]:8080",
+                config={"num_rounds": 1},
+                strategy=strategy,
+            )
+
+            received_pickle_model = dataSocket.recv(BUFLEN * 100)
+            received_model_dict = pickle.loads(received_pickle_model)
+            #print(received_model_dict)
+
+            print(">>> Training Finish")
+
+            update_basic_model(basic_net, received_model_dict, TOTAL_CLIENT_COUNT)
+            print(">>> Model Updated")
+
+        elif command == "Predict":
+            print(">>> Send model to client")
+            basic_net_dict_string = pickle.dumps(basic_net.state_dict())
+            dataSocket.send(basic_net_dict_string)
+
+        dataSocket.close()
+        print(f'>>> Wait for a new Client to connect at port :{PORT}...')
+
+    listenSocket.close()
